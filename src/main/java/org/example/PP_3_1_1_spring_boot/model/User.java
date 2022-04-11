1589f8a153e214372cdd7632a72d75f69c1d7ff8
kosta2011 <kosta4job@yandex.ru>
2022-04-11 12:50:57 +0500

    without jpa and lombok

--- src/main/java/org/example/PP_3_1_1_spring_boot/model/User.java
+++ src/main/java/org/example/PP_3_1_1_spring_boot/model/User.java
@@ -1,17 +1,8 @@
 package org.example.PP_3_1_1_spring_boot.model;
 
 
-import lombok.*;
-
 import javax.persistence.*;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -31,5 +22,66 @@ public class User {
     }
 
 
+    public User() {
 
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        final User other = (User) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (this.getId() != other.getId()) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$lastName = this.getLastName();
+        final Object other$lastName = other.getLastName();
+        if (this$lastName == null ? other$lastName != null : !this$lastName.equals(other$lastName)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof User;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final long $id = this.getId();
+        result = result * PRIME + (int) ($id >>> 32 ^ $id);
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $lastName = this.getLastName();
+        result = result * PRIME + ($lastName == null ? 43 : $lastName.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "User(id=" + this.getId() + ", name=" + this.getName() + ", lastName=" + this.getLastName() + ")";
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
