19ae1e9c35cc35a78a00a4e6b462028b83440939
kosta2011 <kosta4job@yandex.ru>
2022-04-11 12:48:19 +0500

    without jpa and lombok

---
+++ src/main/java/org/example/PP_3_1_1_spring_boot/Pp311SpringBootApplication.java
@@ -0,0 +1,15 @@
+package org.example.PP_3_1_1_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Pp311SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp311SpringBootApplication.class, args);
+	}
+
+}
